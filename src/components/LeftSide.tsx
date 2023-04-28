d0b42e40bcff125530952c5434a726d668df18b9
Jeremy Liu <unswmercury@gmail.com>
2023-04-29 01:38:49 +1000

    Add Navbar

---
+++ src/components/LeftSide.tsx
@@ -0,0 +1,15 @@
+import { TbBrandGithub } from "react-icons/tb";
+import {
+  SlSocialYoutube,
+  SlSocialLinkedin,
+  SlSocialFacebook,
+  SlSocialInstagram,
+} from "react-icons/sl";
+
+const LeftSide = () => {
+  return (
+    <div className="w-full h-full flex flex-col items-center justify-end gap-4 text-textLight"></div>
+  );
+};
+
+export default LeftSide;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,6 @@
-import Navbar from "@/components/Navbar";
 import Head from "next/head";
+import Navbar from "@/components/Navbar";
+import LeftSide from "@/components/LeftSide";
 
 export default function Home() {
   return (
@@ -8,10 +9,17 @@ export default function Home() {
         <title>My Portfolio</title>
         <meta name="description" content="My Portfolio" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href="/logo.ico" />
       </Head>
       <main className="w-full h-screen font-bodyFont bg-bodyColor text-textLight overflow-x-hidden overflow-y-scroll">
         <Navbar />
+        <div className="w-full h-[88vh] xl:flex items-center gap-20 justify-between">
+          <div>
+            <LeftSide />
+          </div>
+          <div></div>
+          <div></div>
+        </div>
       </main>
     </>
   );
